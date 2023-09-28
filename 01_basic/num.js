9532809dde354a7fb22a91de4cfe755c91146120
Sumit-986132 <145793516+Sumit-986132@users.noreply.github.com>
2023-09-28 16:15:43 +0000

    num

---
+++ 01_basic/num.js
@@ -0,0 +1,12 @@
+let score = 400;
+let number = new Number(100);
+console.log(number.toFixed(2));
+console.log(score.toFixed(3));
+
+let balance = new Number(40.775);
+console.log(balance.toString().length);
+console.log(balance.toPrecision(3));
+
+let mark = new Number(10000000);
+console.log(mark.toLocaleString());
+console.log(mark.toLocaleString('en-IN'));
