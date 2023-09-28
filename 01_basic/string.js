f063d1f8727736f5944476c3ecd09f58df6f6d85
Sumit-986132 <145793516+Sumit-986132@users.noreply.github.com>
2023-09-28 15:09:33 +0000

    string

---
+++ 01_basic/string.js
@@ -0,0 +1,9 @@
+let name = "sumeet";
+console.log(name.length);
+console.log(name[2]);
+console.log(name.slice(3, 4));
+console.log(name.substring(3,4));
+
+let myName = "    sumeet    ";
+console.log(myName.trim());
+console.log(myName.replace("ee","i"));
