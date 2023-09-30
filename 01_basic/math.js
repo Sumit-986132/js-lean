25cfb10592c01182f8c2e7ae4c21b7e5d6dc79ef
Sumit-986132 <145793516+Sumit-986132@users.noreply.github.com>
2023-09-30 08:39:58 +0000

    math

---
+++ 01_basic/math.js
@@ -0,0 +1,7 @@
+console.log(Math.random());
+console.log(Math.random()*10 + 1);
+console.log(Math.ceil(Math.random()*10 + 1));
+
+const maxNum = 6;
+const minNum = 1;
+console.log(Math.floor(Math.random()*(maxNum - minNum + 1))+ minNum);
